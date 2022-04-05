e7d48e8976d7de85cede08a0f8749d65285178b8
tudorobretin <tudorobre@gmail.com>
2022-04-05 14:16:08 +0300

    simplified code a bit

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ def define_symbol():
         symbol = "🔺"
 
 
-
 today = dt.datetime.today().date()
 today_weekday = dt.datetime.today().weekday()
 
@@ -108,28 +107,4 @@ if today_weekday in range(0, 5):
 
         getnews()
         send_sms()
-
-
-
-
-#print(data)
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
